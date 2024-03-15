48440b11c08da185b6650275e49cac96986d4775
bsul24 <w.o.sullivan24@gmail.com>
2024-03-15 12:39:23 -0700

    Fix typo

--- app/dashboard/customers/[id]/edit/page.tsx
+++ app/dashboard/customers/[id]/edit/page.tsx
@@ -5,19 +5,17 @@
 import Form from '@/app/ui/customers/edit-form';
 import Breadcrumbs from '@/app/ui/customers/breadcrumbs';
 import { fetchCustomerByID } from '@/app/lib/data';
- 
-export default async function Page({ params }: { params: { id: number} }) {
+
+export default async function Page({ params }: { params: { id: number } }) {
   const id = params.id;
 
-  const [ customer ] = await Promise.all([
-    fetchCustomerByID(id),
-  ]);
-  
+  const [customer] = await Promise.all([fetchCustomerByID(id)]);
+
   return (
     <main>
       <Breadcrumbs
         breadcrumbs={[
-          { label: 'Customer', href: '/dashboard/customers' },
+          { label: 'Customers', href: '/dashboard/customers' },
           {
             label: 'Edit Customer',
             href: `/dashboard/customers/${id}/edit`,
