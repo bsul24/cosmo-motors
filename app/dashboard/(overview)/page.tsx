d5db1580243c95ecfdb9b921108fdd8805c15123
bsul24 <w.o.sullivan24@gmail.com>
2024-05-23 15:08:34 -0700

    Update dashboard text

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,17 +4,29 @@
 
 import { lusitana } from '@/app/ui/fonts';
 
- 
 export default async function Page() {
-
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <p>This webpage is based on the Next.js tutorial starter project available on <strong><a className="text-red-600" href="https://nextjs.org/learn/dashboard-app" color="blue">Project</a></strong>. All further work was made by the members of group 24.</p>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-      </div>
+      <p>
+        This project utilizes some of the starter code from the Next.js website,
+        available{' '}
+        <strong>
+          <a
+            className="text-red-600"
+            href="https://nextjs.org/learn/dashboard-app"
+            target="_blank"
+            rel="noopener noreferrer"
+            color="blue"
+          >
+            here
+          </a>
+        </strong>
+        .
+      </p>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4"></div>
     </main>
   );
 }
